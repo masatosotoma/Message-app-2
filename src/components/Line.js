4e608aec22d6ee1d89e8f706a4b4499faf26ceee
masatosotoma <masatosotoma@gmail.com>
2024-09-22 22:25:25 -0400

    Line component the messages data from firebase

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import SignOut from './SignOut'
+import React, { useEffect, useState } from "react";
+import SignOut from "./SignOut";
+import { db } from "../firebase.js";
+import { ConstructionOutlined } from "@mui/icons-material";
 
 function Line() {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    db.collection("messages")
+      .orderBy("createdAt")
+      .limit(50)
+      .onSnapshot((snapshot) => {
+        setMessages(snapshot.docs.map((doc) => doc.data()));
+      });
+  }, []);
+
   return (
-    <div><SignOut/></div>
-  )
+    <div>
+      {console.log(messages)}
+      <SignOut />
+    </div>
+  );
 }
 
-export default Line
+export default Line;
