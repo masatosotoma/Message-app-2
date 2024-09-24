886c0f10a8b7ad3b2924e43e3d4c2e068c43ad55
masatosotoma <masatosotoma@gmail.com>
2024-09-23 22:08:54 -0400

    Added current user Sent/Receive icons

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import SignOut from "./SignOut";
-import { db } from "../firebase.js";
+import { db, auth } from "../firebase.js";
 import { ConstructionOutlined } from "@mui/icons-material";
 import SendMessage from "./SendMessage";
 
@@ -22,7 +22,12 @@ function Line() {
       <div className="msgs">
         {messages.map(({ id, text, photoURL, uid }) => (
           <div>
-            <div key={id}>
+            <div
+              key={id}
+              className={`msg ${
+                uid === auth.currentUser.uid ? "sent" : "received"
+              }`}
+            >
               <img src={photoURL} />
               <p>{text}</p>
             </div>
